b56388a04e7b538286a1c8c9aade5af37c89d83f
M Huzaifa Nizami <153551455+MHuzaifanizami@users.noreply.github.com>
2024-03-18 22:16:50 +0500

    file uploaded

---
+++ app.js
@@ -0,0 +1,36 @@
+// / ..................... ch 13 - 15 ...................................
+
+// Q no 01 
+
+// var studentName = [];
+
+// Q no 02 
+
+// var  studentName = obj[];
+
+
+// Q no 03 
+
+// var abc = ["Hzaifa"];
+
+
+// Q no 04 
+
+// var num1 = [12];
+
+// Q no 05 
+
+// var azd = [true];
+
+// Q no 06 
+
+// let mixedArray = [42, "apple", true, ];
+
+// Q no 07 
+
+// var education = ["SSC,HSC,BCS,BS,BCOM,MS,M.Phil,PhD"];
+
+// document.write(`<h1>Qualifications</h1> <br> <ol><li> ) SSC </li>
+// <li> ) HSC </li> <li> ) BCS </li> <li> ) BS </li> <li> ) BCOM </li> <li> ) MS </li> <li> ) M.Phil </li> <li> ) PhD </li> </ol> `)
+
+
